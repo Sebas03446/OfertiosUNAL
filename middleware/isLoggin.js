a8c4fa0ddef948d1fc829d2dca6adad3c6ae3ed1
Jean Sebastian French <jsoracipa@unal.edu.co>
2024-05-16 18:00:54 -0500

    fix: login

--- middleware/isLoggin.js
+++ middleware/isLoggin.js
@@ -1,16 +1,8 @@
 export default defineNuxtRouteMiddleware(async () => {
-    const client = useSupabaseClient();
-    const { data, err } = await client.auth.getSession();
-    if (err) {
-        console.log('err', err)
-        return
-        //return navigateTo('/');
-    }
-    const user = data?.session?.user
-    console.log(data)
-    console.log(user)
-    /* if (!user) {
-        return navigateTo('/');
-    } */
+    const user = useSupabaseUser()
+
+  if (!user.value) {
+    return navigateTo('/')
+  }
 });
 
