03531f46c822c7f6749cf02cac3af5abd94b5cc3
Jean Sebastian French <jsoracipa@unal.edu.co>
2024-05-16 17:35:47 -0500

    feat: add logs

--- middleware/isLoggin.js
+++ middleware/isLoggin.js
@@ -2,9 +2,14 @@ export default defineNuxtRouteMiddleware(async () => {
     const client = useSupabaseClient();
     const { data, err } = await client.auth.getSession();
     if (err) {
+        console.log('err', err)
         return navigateTo('/');
     }
     const user = data?.session?.user
+    console.log('user', user)
+    console.log('data', data)
+    console.log('err', err)
+
     if (!user) {
         return navigateTo('/');
     }
